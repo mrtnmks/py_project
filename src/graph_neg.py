fb8b74863e2a05723259b7c3d71f4bd14b6dfe23
Martin <martin@eduroam-196-238.wifi.slu.cz>
2024-11-04 10:31:29 +0100

    macOS updated code

--- src/graph_neg.py
+++ src/graph_neg.py
@@ -2,18 +2,33 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
+
+# Windows execution
+# file_path = '../data/heatmap_neg.xlsx'
+# data = pd.read_excel(file_path)
+
+# macOS path
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(base_dir, '/Users/martin/Documents/projekt/data/heatmap_neg.xlsx')
 
 # Načtení dat z xlsx souboru
-file_path = 'heatmap_neg.xlsx'
 data = pd.read_excel(file_path)
 
 # Nastavení sloupce 'category' jako index
 data.set_index('category', inplace=True)
 
 # Generování heatmapy s převrácenou barevnou škálou
-plt.figure(figsize=(12, 8))
+plt.figure(figsize=(11, 8))
 sns.heatmap(data, annot=True, cmap='coolwarm_r', center=0)
 plt.title('neg model')
 plt.tight_layout()
-plt.savefig('heatmaps/heatmap_n.png')
+
+# Windows execution
+#plt.savefig('heatmaps/heatmap_neg.png')
+
+# macOS execution
+output_path = os.path.join(base_dir, '/Users/martin/Documents/projekt/heatmaps/heatmap_neg.png')
+plt.savefig(output_path)
+
 plt.show()
--- src/graph_pos.py
+++ src/graph_pos.py
@@ -2,18 +2,33 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
+
+# Windows execution
+# file_path = '../data/heatmap_pos.xlsx'
+# data = pd.read_excel(file_path)
+
+# macOS path
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(base_dir, '/Users/martin/Documents/projekt/data/heatmap_pos.xlsx')
 
 # Načtení dat z xlsx souboru
-file_path = 'heatmap_pos.xlsx'
 data = pd.read_excel(file_path)
 
 # Nastavení sloupce 'category' jako index
 data.set_index('category', inplace=True)
 
 # Generování heatmapy s převrácenou barevnou škálou
-plt.figure(figsize=(12, 8))
+plt.figure(figsize=(11, 8))
 sns.heatmap(data, annot=True, cmap='coolwarm_r', center=0)
 plt.title('pos model')
 plt.tight_layout()
-plt.savefig('heatmaps/heatmap_p')
+
+# Windows execution
+#plt.savefig('heatmaps/heatmap_pos.png')
+
+# macOS execution
+output_path = os.path.join(base_dir, '/Users/martin/Documents/projekt/heatmaps/heatmap_pos.png')
+plt.savefig(output_path)
+
 plt.show()
